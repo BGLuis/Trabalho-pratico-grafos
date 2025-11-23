91f7d259a9eecd0ea565d671adf2842cdb6ecc46
jaoppb <joaopedroperes06@gmail.com>
2025-11-23 14:33:54 -0300

    feat: log splitting lines

--- utils.py
+++ utils.py
@@ -59,4 +59,5 @@ def extract_from_key(data: Dict[str, Any], key: str) -> Any:
 
 
 def log(message: str) -> None:
-    print(f"[{datetime.now().isoformat()}] {message}")
+    for line in message.splitlines():
+        print(f"[{datetime.now().isoformat()}] {line}")
